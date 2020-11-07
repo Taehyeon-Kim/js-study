03aa57b1a8be7591c6a78bd8961178741c311259
Taehyeon-Kim <taehyk528@gmail.com>
2020-11-07 18:09:33 +0900

    [FEAT] 마우스로 particle 분리 이벤트(폰트 모양대로 안잡히는 이슈)

--- kinetic-typography/text.js
+++ kinetic-typography/text.js
@@ -1,13 +1,12 @@
 export class Text {
   constructor() {
     this.canvas = document.createElement('canvas');
-    this.canvas.style.position = 'absolute';
-    this.canvas.style.left = '0';
-    this.canvas.style.top = '0';
-    
+    // this.canvas.style.position = 'absolute';
+    // this.canvas.style.left = '0';
+    // this.canvas.style.top = '0';
+    // document.body.appendChild(this.canvas);
+
     this.ctx = this.canvas.getContext('2d');
-    
-    document.body.appendChild(this.canvas);
   }
   
   setText(str, density, stageWidth, stageHeight) {
